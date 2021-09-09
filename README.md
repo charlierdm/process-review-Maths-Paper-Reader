6699bafa3fba8d57910a253e912d92519664b1f4
Charlie <charlie.davismiller@gmail.com>
2021-09-09 15:08:26 +0100

    readme updated

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # Maths Paper Reader 
 
+This is my solution to a process review on 09/09/2021. I have written my solution in JavaScript and used Jest to test.
+
+I wrote the specifications below based upon the information provided on the call, I then created the input/output table and based my tests off of that. I used a TDD approach to build this system, all tests are passing.
+
+![GCSE-Maths-Non-Calculator-Topics](https://user-images.githubusercontent.com/75075773/132701145-c939568f-1c7b-4a3b-8989-7124078e6f6b.jpg)
+
+
+## Specifications
+```
 A company that makes academic maths papers easier to read.
 
 Scans through the maths paper and looks for sums, extracts them as strings.
@@ -9,10 +18,7 @@ It will evaluate the sum strings, then output the original sum string, with the 
 It will contain either 1 or 2 numbers.
 
 example: ['2 + 2', 4]
-
-+ - * /
-
-
+```
 
 ## Input/Output Table
 
@@ -24,5 +30,5 @@ example: ['2 + 2', 4]
 |  '4 - 2'    | ['4 - 2', 2]        |
 |  '2 * 2'    | ['2 * 2', 4]            |
 |  '8 / 4'    | ['8 / 4', 2]            |
-|Edge Case             |             |
+| **Edge Case**             |             |
 |  non-string          | Error("Input must be a string.")      |
--- mathsPaperReader.test.js
+++ mathsPaperReader.test.js
@@ -1,49 +1,33 @@
-const mathsPaperReader = require('./mathsPaperReader')
+const mathsPaperReader = require("./mathsPaperReader");
 
-describe('mathsPaperReader', () => {
-  it('returns a single positive value', () => {
-    expect(mathsPaperReader('1')).toEqual(['1', 1])
-  })
+describe("mathsPaperReader", () => {
+  it("returns a single positive value", () => {
+    expect(mathsPaperReader("1")).toEqual(["1", 1]);
+  });
 
-  it('returns a single negative value', () => {
-    expect(mathsPaperReader('-1')).toEqual(['-1', -1])
-  })
+  it("returns a single negative value", () => {
+    expect(mathsPaperReader("-1")).toEqual(["-1", -1]);
+  });
 
-  it('returns the string with the evaluated some in an array (+)', () => {
-    expect(mathsPaperReader('1 + 1')).toEqual(['1 + 1', 2])
-  })
+  it("returns the string with the evaluated some in an array (+)", () => {
+    expect(mathsPaperReader("1 + 1")).toEqual(["1 + 1", 2]);
+  });
 
-  it('returns the string with the evaluated some in an array (-)', () => {
-    expect(mathsPaperReader('4 - 2')).toEqual(['4 - 2', 2])
-  })
+  it("returns the string with the evaluated some in an array (-)", () => {
+    expect(mathsPaperReader("4 - 2")).toEqual(["4 - 2", 2]);
+  });
 
-  it('returns the string with the evaluated some in an array (*)', () => {
-    expect(mathsPaperReader('2 * 2')).toEqual(['2 * 2', 4])
-  })
+  it("returns the string with the evaluated some in an array (*)", () => {
+    expect(mathsPaperReader("2 * 2")).toEqual(["2 * 2", 4]);
+  });
 
-  it('returns the string with the evaluated some in an array (/)', () => {
-    expect(mathsPaperReader('8 / 4')).toEqual(['8 / 4', 2])
-  })
+  it("returns the string with the evaluated some in an array (/)", () => {
+    expect(mathsPaperReader("8 / 4")).toEqual(["8 / 4", 2]);
+  });
 
-  it('throws an error when given anything other than a string', () => {
+  it("throws an error when given anything other than a string", () => {
     expect(() => {
-      mathsPaperReader(1)
-    }).toThrow("Input must be a string.")
-  })
-
-})
-
-
-
-// ## Input/Output Table
-
-// | Input       | Output      |
-// | ----------- | ----------- |
-// |  '1'        | ['1', 1]    |
-// |  '-1'       | ['-1', -1]  |
-// |  '1 + 1'    | ['1 + 1', 2]            |
-// |  '4 - 2'    | ['4 -2', 2]        |
-// |  '2 * 2'    | ['2 * 2', 4]            |
-// |  '8 / 4'    | ['8 / 4', 2]            |
-// |Edge Case             |             |
-// |  non-string          | Error("Input must be a string.")      |
+      mathsPaperReader(1);
+    }).toThrow("Input must be a string.");
+  });
+});
